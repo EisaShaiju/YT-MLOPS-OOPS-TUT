04f8826ff8e047f94a2cee5b31869b80372f9174
Eisa <eisashaiju@gmail.com>
2025-08-02 17:09:20 +0530

    signup and signin done"

--- oops_prj.py
+++ oops_prj.py
@@ -13,12 +13,36 @@ class chatbook:
                         3. press to message a friend
                         4. press to exit""")
         if user_input =="1":
-            pass
+            self.signup()
         elif user_input =="2":
-            pass
+            self.signin()
         elif user_input =="3":
             pass
         else:
             exit()
 
+    def signup(self):
+        email=input("Enter your email here -> ")
+        pwd=input("setup your password here -> ")
+        self.username=email
+        self.password=pwd
+        print("You have successfully signed up")
+        print("\n")
+        self.menu()
+
+    def signin(self):
+        if self.username==''and self.password=='':
+            print("Please signup by pressing 1 in the main menu")
+        else:
+            uname=input("Please enter your username here->")
+            pwd=input("Please enter your password here->")
+            if(self.username==uname and self.password==pwd):
+                print("You have signed in successfully")
+                self.loggedin=True
+            else :
+                print("Please input correct credentials")
+        print("\n")
+        self.menu()
+
 obj=chatbook()
+
